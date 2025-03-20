f151f833b1c1a6162f094c9eb8fefcb7388598fc
Hnam2107 <23b1001p0145@students.hou.edu.vn>
2025-03-20 14:01:54 +0700

    btvn buổi 7

---
+++ src/vn/com/T3H/B7/Zoo.java
@@ -0,0 +1,32 @@
+package vn.com.T3H.B7;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Zoo
+{
+   ArrayList<Animal> animals = new ArrayList<>();
+
+    public void addAnimal(Animal animal)
+    {
+        animals.add(animal);
+    }
+
+
+    public void displayInfo()
+    {
+        for (Animal animal : animals)
+        {
+            animal.displayInfo();
+            System.out.println("-------------");
+        }
+    }
+
+    public void makeSounds()
+    {
+        for(Animal animal : animals)
+        {
+            animal.makeSound();
+        }
+    }
+}
